760cca4a91691810ec630b7366c93b9f69f180aa
huypqse <huypqse@gmail.com>
2025-10-15 23:29:58 +0700

    feat: add wallet entity

---
+++ wallet_service/src/main/java/com/wallet_svc/wallet/entity/Wallet.java
@@ -0,0 +1,75 @@
+package com.wallet_svc.wallet.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "wallets")
+public class Wallet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Column(name = "user_id", unique = true, nullable = false)
+	Integer userId;
+
+	@Column(precision = 15, scale = 2, nullable = false)
+	@Builder.Default
+	BigDecimal balance = BigDecimal.ZERO;
+
+	@Column(name = "total_spent", precision = 15, scale = 2)
+	@Builder.Default
+	BigDecimal totalSpent = BigDecimal.ZERO;
+
+	@Column(name = "total_earned", precision = 15, scale = 2)
+	@Builder.Default
+	BigDecimal totalEarned = BigDecimal.ZERO;
+
+	@Column(name = "total_refunded", precision = 15, scale = 2)
+	@Builder.Default
+	BigDecimal totalRefunded = BigDecimal.ZERO;
+
+	@Column(length = 10)
+	@Builder.Default
+	String currency = "VND";
+
+	@Column(length = 50)
+	@Builder.Default
+	String status = "ACTIVE"; // ACTIVE / SUSPENDED / CLOSED
+
+	@Column(name = "locked_balance", precision = 15, scale = 2)
+	@Builder.Default
+	BigDecimal lockedBalance = BigDecimal.ZERO;
+
+	@Column(name = "created_at", updatable = false)
+	@Builder.Default
+	LocalDateTime createdAt = LocalDateTime.now();
+
+	@Column(name = "updated_at")
+	@Builder.Default
+	LocalDateTime updatedAt = LocalDateTime.now();
+
+	@PreUpdate
+	public void preUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		if (this.createdAt == null) {
+			this.createdAt = LocalDateTime.now();
+		}
+		this.updatedAt = LocalDateTime.now();
+	}
+}
