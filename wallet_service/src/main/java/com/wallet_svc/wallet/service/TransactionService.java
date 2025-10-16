ac03d7ff2a5ed0f70d9efe152821d1e00d3fc46b
huypqse <huypqse@gmail.com>
2025-10-16 10:13:21 +0700

    feat: add transaction service

---
+++ wallet_service/src/main/java/com/wallet_svc/wallet/service/TransactionService.java
@@ -0,0 +1,14 @@
+package com.wallet_svc.wallet.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.wallet_svc.wallet.dto.response.TransactionResponse;
+
+public interface TransactionService {
+	Page<TransactionResponse> getMyTransactions(Integer userId, Pageable pageable);
+
+	TransactionResponse getTransactionById(Long transactionId);
+
+	Page<TransactionResponse> getAllTransactions(Pageable pageable);
+}
