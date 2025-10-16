9ae50b487707e33d9acd2cb967053fb56e506a02
huypqse <huypqse@gmail.com>
2025-10-16 09:54:42 +0700

    feat: add constant for transaction and wallet status

---
+++ wallet_service/src/main/java/com/wallet_svc/wallet/constant/TransactionStatus.java
@@ -0,0 +1,10 @@
+package com.wallet_svc.wallet.constant;
+
+public class TransactionStatus {
+	public static final String PENDING = "PENDING";
+	public static final String PROCESSING = "PROCESSING";
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAILED = "FAILED";
+	public static final String CANCELLED = "CANCELLED";
+	public static final String REVERSED = "REVERSED";
+}
