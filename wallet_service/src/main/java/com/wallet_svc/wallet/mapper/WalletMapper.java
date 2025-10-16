da340430d4b0144db70aa616acc55ce0703b6798
huypqse <huypqse@gmail.com>
2025-10-16 10:18:19 +0700

    feat: add wallet mapper

---
+++ wallet_service/src/main/java/com/wallet_svc/wallet/mapper/WalletMapper.java
@@ -0,0 +1,17 @@
+package com.wallet_svc.wallet.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.wallet_svc.wallet.dto.response.*;
+import com.wallet_svc.wallet.entity.*;
+
+@Mapper(componentModel = "spring")
+public interface WalletMapper {
+	WalletResponse toWalletResponse(Wallet wallet);
+
+	TransactionResponse toTransactionResponse(WalletTransaction transaction);
+
+	CreditPackageResponse toCreditPackageResponse(CreditPackage creditPackage);
+
+	UsageLogResponse toUsageLogResponse(CreditUsageLog usageLog);
+}
