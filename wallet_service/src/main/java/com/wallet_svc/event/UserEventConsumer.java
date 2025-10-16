1923af50a06e32cc3e833ff38e103e43ffeee6f9
huypqse <huypqse@gmail.com>
2025-10-16 10:20:56 +0700

    feat: add handle for user event

---
+++ wallet_service/src/main/java/com/wallet_svc/event/UserEventConsumer.java
@@ -0,0 +1,32 @@
+package com.wallet_svc.event;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.wallet_svc.wallet.service.WalletService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class UserEventConsumer {
+	WalletService walletService;
+
+	@KafkaListener(topics = "user.registered", groupId = "${spring.kafka.consumer.group-id}")
+	public void handleUserRegistered(UserRegisteredEvent event) {
+		log.info("Received user.registered event for user: {}", event.getUserId());
+
+		try {
+			walletService.createWallet(event.getUserId());
+			log.info("Wallet created for user: {}", event.getUserId());
+		} catch (Exception e) {
+			log.error("Failed to create wallet for user: {}", event.getUserId(), e);
+			// In production, implement retry logic or dead letter queue
+		}
+	}
+}
---
+++ wallet_service/src/main/java/com/wallet_svc/event/UserRegisteredEvent.java
@@ -0,0 +1,28 @@
+package com.wallet_svc.event;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisteredEvent {
+	@JsonProperty("user_id")
+	private Integer userId;
+
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("username")
+	private String username;
+
+	@JsonProperty("timestamp")
+	private LocalDateTime timestamp;
+}
